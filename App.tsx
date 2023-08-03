487a2de309455ff562368e6bd65515e70122be19
Guilherme Sandi <guilherme_sandi@hotmail.com>
2023-08-03 19:41:02 -0300

    Instalando o OneSignal no Projeto

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+const { ONESIGNAL_KEY } = process.env;
 
 import { Routes } from './src/routes';
 
@@ -8,6 +11,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId(ONESIGNAL_KEY);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
