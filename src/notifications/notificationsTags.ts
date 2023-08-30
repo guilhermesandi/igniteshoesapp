37cbf6e75356e34486027037f01e89c9575ba1d4
Guilherme Sandi <guilherme_sandi@hotmail.com>
2023-08-30 18:44:58 -0300

    Criando tag para carrinho

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -6,3 +6,7 @@ export function tagUserInfoCreate() {
     'user_email': 'guilherme_sandi@hotmail.com'
   });
 }
+
+export function tagCartUpdate(itemsCount: string) {
+  OneSignal.sendTag('cart_items_count', itemsCount);
+}
